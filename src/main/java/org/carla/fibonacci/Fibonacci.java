8e8ca83c2e48c1df026b4c74600673b764456ad6
carlaserracant <80315096+carlaserracant@users.noreply.github.com>
2022-03-09 10:38:53 +0100

    Test devuelve 2 si el número es 3

--- src/main/java/org/carla/fibonacci/Fibonacci.java
+++ src/main/java/org/carla/fibonacci/Fibonacci.java
@@ -1,4 +1,25 @@
 package org.carla.fibonacci;
 
 public class Fibonacci {
+
+    int fibonacci(int n)
+    {
+        int fib;
+        if (n>1){
+            fib = fibonacci(n-1) + fibonacci(n-2);  //función recursiva
+        }
+        else if (n==1) {  // caso base
+            fib = 1;
+        }
+        else if (n==0){  // caso base
+            fib = 0;
+        }
+        else{ //error
+            System.out.println("Debes ingresar un tamaño mayor o igual a 1");
+            fib = -1;
+        }
+
+        return fib;
+    }
+
 }
--- src/test/java/org/carla/fibonacci/FibonacciTest.java
+++ src/test/java/org/carla/fibonacci/FibonacciTest.java
@@ -1,5 +1,38 @@
 package org.carla.fibonacci;
 
-class FibonacciTest {
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class FibonacciTest {
+    private Fibonacci fibonacci;
+    @BeforeEach
+    private void setup() { fibonacci = new Fibonacci(); }
+
+    @AfterEach
+    private void finish() { fibonacci = null; }
+
+    @Test
+    public void testReturnZeroIfTheNumberIsZero() {
+        int expectedValue = 0;
+        int obtainedValue = fibonacci.fibonacci(0);
+        assertEquals(expectedValue,obtainedValue);
+    }
+
+    @Test
+    public void testReturnOneIfTheNumberIsOne() {
+        int expectedValue = 1;
+        int obtainedValue = fibonacci.fibonacci(1);
+        assertEquals(expectedValue,obtainedValue);
+    }
+
+    @Test
+    public void testReturnTwoIfTheNumberIsThree() {
+        int expectedValue = 2;
+        int obtainedValue = fibonacci.fibonacci(3);
+        assertEquals(expectedValue,obtainedValue);
+    }
 }
